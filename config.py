564f76e0e274e53385197073b10db788105cc544
KrishnaKV2004 <krishnaverma2004kvaudi@gmail.com>
2025-07-20 23:23:57 +0530

    Add Congiguration

--- config.py
+++ config.py
@@ -7,6 +7,12 @@ SCREEN_WIDTH, SCREEN_HEIGHT = user32.GetSystemMetrics(0), user32.GetSystemMetric
 # Camera resolution
 CAM_WIDTH, CAM_HEIGHT = 640, 480
 
-# Gesture control tuning
+# Cursor smoothing and click delay
 SMOOTHING_FACTOR = 5
 CLICK_DELAY = 0.3
+
+# Gesture control tuning
+FRAME_THRESHOLD = 5                 # Frames for stable gesture (right click)
+CLICK_DIST_THRESHOLD = 25           # Distance threshold for pinch gestures
+CLICK_HYSTERESIS = 3                # Buffer to reduce flicker
+DRAG_HOLD_DELAY = 0.4               # Seconds to hold pinch before triggering drag
